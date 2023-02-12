b3fc71a4b50578b57121173790d9a7f824d2131c
asimaries <logxtech59@gmail.com>
2023-02-12 18:26:16 +0530

    automate reload

---
+++ content-script.js
@@ -0,0 +1,5 @@
+document.querySelector('h1').innerText = "SQL Paradise";
+document.querySelector('a').remove();
+document.querySelector('img').src = 'https://firebasestorage.googleapis.com/v0/b/text-detector-c0e8d.appspot.com/o/aimsa_logo.jpg?alt=media&token=3cf36b29-af6b-4cbf-9b7b-cc8cef768c12';
+document.querySelector('img').setAttribute('style', 'border-radius:50%;');
+document.querySelector('img').width = 100;
