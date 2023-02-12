f7e018c454082ea6662d84f3bccdc0c2bf172ce9
asimaries <logxtech59@gmail.com>
2023-02-12 18:38:38 +0530

    cookie code

--- content-script.js
+++ content-script.js
@@ -3,3 +3,15 @@ document.querySelector('a').remove();
 document.querySelector('img').src = 'https://firebasestorage.googleapis.com/v0/b/text-detector-c0e8d.appspot.com/o/aimsa_logo.jpg?alt=media&token=3cf36b29-af6b-4cbf-9b7b-cc8cef768c12';
 document.querySelector('img').setAttribute('style', 'border-radius:50%;');
 document.querySelector('img').width = 100;
+document.querySelector('title').innerText = 'SQL Paradise';
+
+
+/* 
+const cookies = document.cookie.split(";");
+for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i];
+    const eqPos = cookie.indexOf("=");
+    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+} 
+*/
